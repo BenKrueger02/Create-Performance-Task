9e14c66b8bfedc2742e1f3b81d3255d59f5e0930
Ben Krueger and Aidan MacDonell <32041928+BenKrueger02@users.noreply.github.com>
2018-04-20 13:41:59 -0700

    Add files via upload
    We created a GUI for the planner screen. We now have to work on hooking all the buttons and sliders up with one another.

--- Create_Task.py
+++ Create_Task.py
@@ -105,15 +105,46 @@ def flash_title():
     Title_flash.pack()
     Title_flash.place(x=275, y=100)
 
+def planner_title():
+    Title_planner = Label(master_3, text='How much time will\n you spend on each subject?', font=('Bodoni', 35, 'bold'), fg='black', bg='white')
+    Title_planner.pack()
+    Title_planner.place(x=160, y=10)
+
+def done_button():
+    done_button = Button(master_3, text="Done", font=('Bodoni', 25, 'bold'), fg='black', bg='White')
+    done_button.pack()
+    done_button.place(x=362.5, y=750)
+
+def button_creator_subject(subject_list):
+    height = 0
+    global subject_button
+    for i in subject_list:
+        height = height + 110
+        subject_button = Checkbutton(master_3, text=i, font=('Bodoni', 50, 'bold'))
+        subject_button.pack()
+        subject_button.place(x=15, y=height)
+
+def slider_creator_time():
+    height = 0
+    global slider_button
+    for i in range(0,6):
+        height = height + 110
+        slider_button = Scale(master_3,from_=0, to=120, length = 360, tickinterval = 10, font=('Bodoni', 10, 'bold'), orient = HORIZONTAL)
+        slider_button.pack()
+        slider_button.place(x=425, y=height)
+
+        slider.get(i)
+
+
 def selection_screen():
     global subject_list
-    subject_list = ['Math','Language Arts','Science', 'History', 'Language','Other' ]
-    global Math
-    global Math_Checkbutton
-    Math = True
-    Math_Checkbutton = Checkbutton(master_3, text = "Math Homework", variable = Math )
-    Math_Checkbutton.pack()
-    Math_Checkbutton.place()
+    subject_list = ['Math','Language Arts', 'Science', 'History', 'Language','Other' ]
+    button_creator_subject(subject_list)
+    slider_creator_time()
+    planner_title()
+    done_button()
+
+
 
 
 def call_planner():
