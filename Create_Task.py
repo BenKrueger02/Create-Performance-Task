b6170e933af3d36b5616929560beb030f3dd7a6c
Ben Krueger and Aidan MacDonell <32041928+BenKrueger02@users.noreply.github.com>
2018-04-23 13:50:24 -0700

    Add files via upload
    Started working on the algorithm that displays clock time.

--- Create_Task.py
+++ Create_Task.py
@@ -13,6 +13,12 @@ global next_button
 next_button = True
 
 
+math = False
+la = False
+science = False
+history = False
+language = False
+other = False
 
 def questions():
     global word_count
@@ -49,8 +55,6 @@ def flip_command():
     if next_button == True:
         next_button = False
         Word.destroy()
-
-
         flash_title()
         flash_sequence()
     else:
@@ -60,9 +64,30 @@ def flip_command():
         flash_sequence()
 
 def done_command():
-    for i in range(0,6):
-        slider_button.destroy()
-        subject_button.destroy()
+    global hour
+    global minute
+    global schedule_canvas
+    global master_4
+    master_4 = Tk()
+    hour = input('Enter the hour you want to start:')
+    if (0 >= hour) or (hour > 12) or (type(hour) != int):
+        print('Invalid input. Try again.')
+        done_command()
+    minute = input('Enter the minute you want to start as a two digit number:')
+    if (0 > minute) or (minute > 59) or (type(minute) != int):
+        print('Invalid input. Try again.')
+        done_command()
+    else:
+        if minute <10:
+            print (str(hour) + ':0' + str(minute))
+        else:
+            print (str(hour) + ':' + str(minute))
+    schedule_canvas = Canvas(master_4, width=800, height=800, bg='white')
+    schedule_canvas.pack()
+    create_schedule()
+    create_planner(subjects_with_hw)
+    clock_time(time_for_subjects)
+
 
 
 def flash_sequence():
@@ -123,40 +148,138 @@ def done_button():
     done_button.pack()
     done_button.place(x=362.5, y=750)
 
-def button_creator_subject(subject_list):
-    height = 0
-    global subject_button
-    for i in subject_list:
-        height = height + 110
-        subject_button = Checkbutton(master_3, text=i, font=('Bodoni', 50, 'bold'))
-        subject_button.pack()
-        subject_button.place(x=15, y=height)
+
+def label_creator_subject(subject_list):
+     height = 0
+     global subject_label
+     for i in subject_list:
+         height = height + 110
+         subject_label = Label(master_3, text=i, font=('Bodoni', 25, 'bold'), fg = 'black', bg = 'white')
+         subject_label.pack()
+         subject_label.place(x=15, y=height)
 
 def slider_creator_time():
     height = 0
-    global slider_button
-    for i in range(0,6):
-        height = height + 110
-        slider_button = Scale(master_3,from_=0, to=120, length = 360, tickinterval = 10, font=('Bodoni', 10, 'bold'), orient = HORIZONTAL)
-        slider_button.pack()
-        slider_button.place(x=425, y=height)
+    global slider_button_math
+    global slider_button_Language_arts
+    global slider_button_science
+    global slider_button_history
+    global slider_button_language
+    global slider_button_other
+
+    height = height + 110
+    slider_button_math = Scale(master_3,from_=0, to=120, length = 360, tickinterval = 10, font=('Bodoni', 10, 'bold'), orient = HORIZONTAL)
+    slider_button_math.pack()
+    slider_button_math.place(x=425, y=height)
+
+    height = height + 110
+    slider_button_Language_arts = Scale(master_3, from_=0, to=120, length=360, tickinterval=10, font=('Bodoni', 10, 'bold'), orient=HORIZONTAL)
+    slider_button_Language_arts.pack()
+    slider_button_Language_arts.place(x=425, y=height)
+
+    height = height + 110
+    slider_button_science = Scale(master_3, from_=0, to=120, length=360, tickinterval=10, font=('Bodoni', 10, 'bold'), orient=HORIZONTAL)
+    slider_button_science.pack()
+    slider_button_science.place(x=425, y=height)
+
+    height = height + 110
+    slider_button_history = Scale(master_3, from_=0, to=120, length=360, tickinterval=10, font=('Bodoni', 10, 'bold'), orient=HORIZONTAL)
+    slider_button_history.pack()
+    slider_button_history.place(x=425, y=height)
+
+    height = height + 110
+    slider_button_language = Scale(master_3, from_=0, to=120, length=360, tickinterval=10, font=('Bodoni', 10, 'bold'), orient=HORIZONTAL)
+    slider_button_language.pack()
+    slider_button_language.place(x=425, y=height)
+
+    height = height + 110
+    slider_button_other = Scale(master_3, from_=0, to=120, length=360, tickinterval=10, font=('Bodoni', 10, 'bold'), orient=HORIZONTAL)
+    slider_button_other.pack()
+    slider_button_other.place(x=425, y=height)
+
+def create_schedule():
+    global subjects_with_hw
+    global time_for_subjects
+    subjects_with_hw = []
+    time_for_subjects = []
+    if slider_button_math.get() > 0:
+        print('You have ' + str(slider_button_math.get()) + ' minutes of Math.')
+        subjects_with_hw.append('Math')
+        time_for_subjects.append(slider_button_math.get())
+    if slider_button_Language_arts.get() > 0:
+        print('You have ' + str(slider_button_Language_arts.get()) + ' minutes of Language Arts.')
+        subjects_with_hw.append('Language Arts')
+        time_for_subjects.append(slider_button_Language_arts.get())
+    if slider_button_science.get() > 0:
+        print('You have ' + str(slider_button_science.get()) + ' minutes of Science.')
+        subjects_with_hw.append('Science')
+        time_for_subjects.append(slider_button_science.get())
+    if slider_button_history.get() > 0:
+        print('You have ' + str(slider_button_history.get()) + ' minutes of History.')
+        subjects_with_hw.append('History')
+        time_for_subjects.append(slider_button_history.get())
+    if slider_button_language.get() > 0:
+        print('You have ' + str(slider_button_language.get()) + ' minutes of Language.')
+        subjects_with_hw.append('Language')
+        time_for_subjects.append(slider_button_language.get())
+    if slider_button_other.get() > 0:
+        print('You have ' + str(slider_button_other.get()) + ' minutes of Somthing else.')
+        subjects_with_hw.append('Other')
+        time_for_subjects.append(slider_button_other.get())
+
+    print(subjects_with_hw)
+    print(time_for_subjects)
+
+def clock_time(time_for_subjects):
+    global time
+    global time_label
+    global time_hour
+    global time_minute
+    global time_clock_label
+    height = 0
+    time_clock_label = Label(master_4, text=hour + minute, font=('Bodoni', 25, 'bold'), fg='black', bg='white')
+    time_clock_label.pack()
+    time_clock_label.place(x=400, y=110)
+
+  #  for x in range(0, len(time_for_subjects)):
+   #     time_hour = time_for_subjects[x]/60
+    #    time_minute = time_for_subjects[x]%60
+
+
+    for i in range(0,len(time_for_subjects)):
+        height = height +110
+        time = time_for_subjects[i]
+        time_label = Label(master_4, text=(str(time) + ' minutes'), font=('Bodoni', 25, 'bold'), fg='black', bg='white')
+        time_label.pack()
+        time_label.place(x=350, y=height)
 
 
 def selection_screen():
     global subject_list
-    subject_list = ['Math','Language Arts', 'Science', 'History', 'Language','Other' ]
-    button_creator_subject(subject_list)
+    global call_back_list
+    subject_list = ['Math','Language Arts', 'Science', 'History', 'Language','Other']
+    label_creator_subject(subject_list)
     slider_creator_time()
     planner_title()
     done_button()
 
-
+def create_planner(subjects_with_hw):
+    planner_label = Label(master_4, text='Schedule', font=('Bodoni', 60, 'bold'), fg='black', bg='white')
+    planner_label.pack()
+    planner_label.place(x=255, y = 10)
+    height = 0
+    for x in subjects_with_hw:
+        height = height + 110
+        schedule_label = Label(master_4, text=x, font=('Bodoni', 25, 'bold'), fg='black', bg='white')
+        schedule_label.pack()
+        schedule_label.place(x=15, y=height)
 
 
 def call_planner():
     global master_3
     global planner_canvas
-    master_3= Tk()
+    global math_time
+    master_3 = Tk()
     planner_canvas = Canvas(master_3, width=800, height=800, bg='white')
     planner_canvas.pack()
     selection_screen()
@@ -174,6 +297,7 @@ flashcard_button.place(x=320, y=300)
 planner_button = Button(master, text="Planner", font = ('Bodoni', 25, 'bold'   ), fg='black', bg='green', command = call_planner)
 planner_button.pack()
 planner_button.place(x=340, y=400)
+
 
 
 
